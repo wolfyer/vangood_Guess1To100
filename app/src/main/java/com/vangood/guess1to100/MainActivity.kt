b0fcb6f4ae82fa8bd481b0874704c374668bb3f2
Class <a90023y@gmail.com>
2022-03-04 09:54:22 +0800

    2ND can use

--- app/src/main/java/com/vangood/guess1to100/MainActivity.kt
+++ app/src/main/java/com/vangood/guess1to100/MainActivity.kt
@@ -2,6 +2,7 @@ package com.vangood.guess1to100
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.vangood.guess1to100.databinding.ActivityMainBinding
 
@@ -18,25 +19,34 @@ class MainActivity : AppCompatActivity() {
     var min = 1
     var max = 100
     var count = 1
-    var box = binding.edTextview.text.toString()
+    var box :String =""
+    //var box = binding.edTextview.text.toString()
     fun startgame(view: View){
         var num : Int = binding.edNum.text.toString().toInt()
-        if (num < ans){
+        if (num < ans && min <= num){
             min = num
             box = "Is Bigger Than $num. Please try again.\n" +
                     "Select a number in $min to $max"
+            binding.edTextview.text = box
             count = count+1
-        }else if (num > ans){
+        }else if (num > ans && max >= num){
             max =num
             box = "Is Smaller Than $num. Please try again.\n" +
                     "Select a number in $min to $max"
+            binding.edTextview.text = box
             count = count+1
         }else if (num == ans){
-            box = "Good Job! The Answer is $num \n You Spend $count Time To Win The Game."
-            var ans = (1..100).random()
+            box = "Good Job! The Answer is $num \n You Spend $count Time To Win The Game."+
+                    "\nPause Bottom to start a new round"
+            binding.edTextview.text = box
+            ans = (1..100).random()
+            min = 1
+            max = 100
+            count = 1
 
         }else{
-            box = "Wrong ! Please try again."
+            box = "Wrong ! Please try again.\nSelect a number in $min to $max"
+            binding.edTextview.text = box
             count = count+1
         }
 
